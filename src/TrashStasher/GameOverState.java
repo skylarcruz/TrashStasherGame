1b1e9a1c5f9d4947444341cdbe5d70609abe712d
Skylar Cruz <skylar.cruz@wsu.edu>
2019-10-06 16:02:30 -0700

    Main Menu + High Scores
    Simple Main Menu and Persistent High Scores. Can now select Raccoon skin from main menu and see high scores for all levels at game over screen

--- src/TrashStasher/GameOverState.java
+++ src/TrashStasher/GameOverState.java
@@ -11,6 +11,10 @@ import org.newdawn.slick.state.StateBasedGame;
 class GameOverState extends BasicGameState {
 	
 	int countdown = 300;
+	int userScore = 0;
+	boolean scoreChecked = false;
+	boolean scoreReplace = false;
+	int scoreArr[];
 	
 	@Override
 	public void init(GameContainer container, StateBasedGame game)
@@ -21,7 +25,16 @@ class GameOverState extends BasicGameState {
 	public void enter(GameContainer container, StateBasedGame game) {
 		container.setSoundOn(true);
 		
-		countdown = 300;
+		TrashStasherGame tsg = (TrashStasherGame)game;
+		
+		TrashStasherGame.getHighScores(
+				TrashStasherGame.LVL1_SCORES_TXT, tsg.highScore1);
+		TrashStasherGame.getHighScores(
+				TrashStasherGame.LVL2_SCORES_TXT, tsg.highScore2);
+		TrashStasherGame.getHighScores(
+				TrashStasherGame.LVL3_SCORES_TXT, tsg.highScore3);
+		
+		getScoreArr();
 		
 	}
 	
@@ -29,22 +42,86 @@ class GameOverState extends BasicGameState {
 	public void render(GameContainer container, StateBasedGame game,
 			Graphics g) throws SlickException {
 		//CryptCaperGame ccg = (CryptCaperGame)game;
+		TrashStasherGame tsg = (TrashStasherGame)game;
 		
 		g.drawString("Game Over", 10, 30);
+		
+		for (int i = 0; i < 10; i ++) {
+			g.drawString(Integer.toString(tsg.highScore1[i]), 300, 300 + i * 50);
+			g.drawString(Integer.toString(tsg.highScore2[i]), 600, 300 + i * 50);
+			g.drawString(Integer.toString(tsg.highScore2[i]), 900, 300 + i * 50);
+		}
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game,
 			int delta) throws SlickException {
 
-		TrashStasherGame ccg = (TrashStasherGame)game;
+		TrashStasherGame tsg = (TrashStasherGame)game;
 		
-		countdown -= 1;
-		ccg.lives = 3;
+		int lineNum = 10;
+		if (scoreChecked == false) {
+			for (int i = 0; i < 10; i++) {
+				if (userScore > scoreArr[i]) {
+					lineNum = i;
+					scoreReplace = true;
+					break;
+				}
+			}
+			if (scoreReplace == true) {
+				setTextScores(lineNum);
+			}
+			scoreChecked = true;
+			TrashStasherGame.getHighScores(
+					TrashStasherGame.LVL1_SCORES_TXT, tsg.highScore1);
+			TrashStasherGame.getHighScores(
+					TrashStasherGame.LVL2_SCORES_TXT, tsg.highScore2);
+			TrashStasherGame.getHighScores(
+					TrashStasherGame.LVL3_SCORES_TXT, tsg.highScore3);
+		} 
+		else
+			countdown -=1;
 		
-		if (countdown <= 0)
-			game.enterState(TrashStasherGame.PLAYINGSTATE);
 		
+		
+		if (countdown <= 0) {
+			tsg.lives = 3;
+			scoreChecked = false;
+			scoreReplace = false;
+			userScore = 0;
+			countdown = 300;
+			game.enterState(TrashStasherGame.STARTUPSTATE);
+		}
+		
+	}
+	
+	private void getScoreArr() {
+		scoreArr = new int[10];
+		
+		if (TrashStasherGame.currLevel == 1)
+			scoreArr = TrashStasherGame.highScore1.clone();
+		if (TrashStasherGame.currLevel == 2)
+			scoreArr = TrashStasherGame.highScore2.clone();
+		if (TrashStasherGame.currLevel == 3)
+			scoreArr = TrashStasherGame.highScore3.clone();
+	}
+	
+	private void setTextScores(int line) {
+		for (int i = 9; i > line; i --) {
+			scoreArr[i] = scoreArr [i - 1];
+		}
+		scoreArr[line] = userScore;
+		
+		if (TrashStasherGame.currLevel == 1)
+			TrashStasherGame.setHighScores(TrashStasherGame.LVL1_SCORES_TXT, scoreArr);
+		if (TrashStasherGame.currLevel == 2)
+			TrashStasherGame.setHighScores(TrashStasherGame.LVL2_SCORES_TXT, scoreArr);
+		if (TrashStasherGame.currLevel == 3)
+			TrashStasherGame.setHighScores(TrashStasherGame.LVL3_SCORES_TXT, scoreArr);
+	}
+	
+	public void setUserScore(int s) {
+		userScore = s;
 	}
 
 	@Override
